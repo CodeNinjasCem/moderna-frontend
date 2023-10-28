db3606e56c5d262bdbe186eb6c934379aaffe2c7
Angel Garcia <angel_012912@hotmail.com>
2023-10-28 00:07:34 -0600

    ListaReceta page generada

--- screens/ListaReceta.jsx
+++ screens/ListaReceta.jsx
@@ -1,16 +1,29 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import ClasificadoComponent from "../components/ClasificadoComponent";
+import ColumnRows from "../components/ColumnRows";
 import React from "react";
+import colors from "../constants/colors";
 
 const ListaReceta = (props) => {
-    const image = props.route.params.params.image;
+  const image = props.route.params.params.image;
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View style={
         {
-        image ? <ClasificadoComponent image={image} /> : <Text>Plan Semanal Dia seleccionado</Text>
+          height: "100%",
+          marginTop: 24,
+          gap: 12,
         }
-      <View>
-        <Text>ListaReceta</Text>
+      }>
+        {image ? (
+          <ClasificadoComponent image={image} />
+        ) : (
+          <Text>Plan Semanal Dia seleccionado</Text>
+        )}
+        <View style={styles.dataContainer}>
+          <Text style={styles.headerRecetas}>Seleccione una receta</Text>
+          <ColumnRows />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -18,4 +31,16 @@ const ListaReceta = (props) => {
 
 export default ListaReceta;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  dataContainer: {
+    gap: 24,
+    paddingVertical: 24,
+    paddingHorizontal: 12,
+    height: "100%",
+  },
+  headerRecetas: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: colors.SECONDARY,
+  },
+});
