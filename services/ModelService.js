4d99f3f4a206c5c23c3fd573bf32981d7f5936c8
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-10-28 00:26:21 -0600

    Servicio de clasificar

---
+++ services/ModelService.js
@@ -0,0 +1,19 @@
+import { MODEL_BASE_URL } from "../config";
+import axios from "axios";
+
+export const classifyImage = async (base64Image) => {
+    const res = await axios
+      .post(`${MODEL_BASE_URL}/classifyImage`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        imagen: base64Image,
+        scaleWidth: 0,
+        scaleHeight: 0,
+      })
+      .then((res) => {
+        return res.data;
+      });
+    
+    return res.message;
+  }
